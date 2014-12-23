fb6f22ff21c0d8dd7cbf44b4de426058caffce46
Scott Burleigh <sburleigh@lindseysoftware.com>
2014-12-23 16:57:17 -0600

    fixed field-goal to consider accuracy

--- Fieldgoal.cpp
+++ Fieldgoal.cpp
@@ -1,7 +1,7 @@
 /*********************************************************************
  * ** Function: 	Fieldgoal.cpp
  * ** Description:	Calculates the length of a fieldgoal, if it's long enough, then if it is accurate enough
- * ** Parameters: 
+ * ** Parameters:
  * ** Pre-Conditions:	Kicker's kick power and accuracy
  * ** Post-Conditions:	Returns a 1 for if fieldgoal is good and 0 if not
  * *********************************************************************/
@@ -18,7 +18,7 @@ int Fieldgoal(Athlete k, int pos){
 
 	x = rand()%99+1;
 
-	a = 5 * tan((x-50)/(31.4159))+60*k.getKickPower()/100;
+	a = 5 * tan((x-50)/(31.4159))+60*k.getKickAccuracy()/100;
 
 	if(a < pos) return 0;// not enough kick accuracy then return 0
 
